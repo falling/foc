8f2686b155c8000b46c452a6a87036abbddfe228
falling <zyz635857938@gmail.com>
2017-12-24 15:07:39 +0800

    sql

---
+++ sql/qj.sql
@@ -0,0 +1,13 @@
+create table qj (
+ o_id               int unsigned          NOT NULL,
+ qj_id               int unsigned         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ ch_name             varchar(20)          DEFAULT ' ',
+ sex                 varchar(5)           DEFAULT ' ',
+ ethnicity           varchar(10)          DEFAULT ' ',
+ id_num              varchar(20)          DEFAULT ' ', 
+ relation            varchar(10)          DEFAULT ' ', 
+ tel                 varchar(20)          DEFAULT ' ',
+ te2                 varchar(20)          DEFAULT ' ',
+ del                 varchar(5)           DEFAULT ' '
+
+)
---
+++ sql/user.sql
@@ -0,0 +1,8 @@
+create table user (
+ username            varchar(10)          DEFAULT ' ',
+ pwd              varchar(20)          DEFAULT ' ',
+ name             varchar(20)          DEFAULT ' ',
+ power            varchar(20)          DEFAULT ' ',
+ remarks             varchar(100)         DEFAULT ' ',
+ del                 varchar(5)           DEFAULT ' ' 
+)
