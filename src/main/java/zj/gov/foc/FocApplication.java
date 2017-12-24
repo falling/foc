04f6ce349128a1699015439bb1ef4772226ed8fa
falling <zyz635857938@gmail.com>
2017-12-24 18:26:27 +0800

    404 500 router

--- src/main/java/zj/gov/foc/FocApplication.java
+++ src/main/java/zj/gov/foc/FocApplication.java
@@ -2,11 +2,25 @@ package zj.gov.foc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class FocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FocApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+		return (container -> {
+			ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+			ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+			container.addErrorPages(error404Page, error500Page);
+		});
 	}
 }
